cba7c2d80b83fcbf32361114de92fb952c768131
Arya More <aryamore2020@gmail.com>
2026-06-28 03:02:31 +0530

    Added menu display and user input

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Welcome to Expense Tracker")
+def display_menu():
+    print("==============================")
+    print("      Expense Tracker")
+    print("==============================")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Delete Expense")
+    print("4. Monthly Summary")
+    print("5. Category Summary")
+    print("6. Exit")
+    
+
+
+display_menu()
+    
+
+choice = input("Enter your choice: ")
+print("You entered:", choice)
